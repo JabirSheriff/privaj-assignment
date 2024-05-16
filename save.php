23f7e1562d70187a573746e61d29a66f4ac96a34
Jabir Sheriff <71977353+JabirSheriff@users.noreply.github.com>
2024-05-17 00:49:16 +0530

    save.php

--- save.php
+++ save.php
@@ -3,8 +3,8 @@
 	$email = $_POST['Email'];
 	$message = $_POST['Message'];
 
-	// Database connection
-	$conn = new mysqli('localhost','root','Jabu2k@2002','contact_db');
+
+	$conn = new mysqli('localhost','root','','contact_db');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
